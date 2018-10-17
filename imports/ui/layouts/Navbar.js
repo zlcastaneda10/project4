291324703185b1e10cc0dd4cd4dfe5b4fffa9dcd
Juan Pablo Gonzalez <jp.gonzalez14@uniandes.edu.co>
2018-10-17 17:43:24 -0500

    navbar

--- imports/ui/layouts/Navbar.js
+++ imports/ui/layouts/Navbar.js
@@ -11,22 +11,21 @@ export default class NavBar extends React.Component {
   render() {
     if (Meteor.userId()) {
       return (
-        <div>
+        <div className='container-fluid'>
         <nav className="navbar navbar-expand-lg navbar-light backcolonav">
-          <Link className="navbar-brand" to="/"><span className='ctext'>Doodles Uniandes</span></Link>
+          <Link className="navbar-brand" to="/"><img className="logo" src="/logo.png" width="340" height="70" className="d-inline-block align-top" alt="Image text"/></Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
+              <li className="nav-item px-4">
                 <Link className="nav-link" to='/doodles'><span className='ctext'>Hitos</span></Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item px-4">
                 <Link className="nav-link" to='/doodlesforms'><span className='ctext'>Crear Hito</span></Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item px-4">
                 <span className="nav-link" onClick={this.onLogout.bind(this)}><span className='ctext'>Logout</span></span>
               </li>
             </ul>
@@ -36,25 +35,25 @@ export default class NavBar extends React.Component {
       );
     } else {
       return (
-        <div>
+        <div className='container-fluid'>
         <nav className="navbar navbar-expand-lg navbar-light backcolonav">
-          <Link className="navbar-brand" to="/"><span className='ctext'>Doodles Uniandes</span></Link>
+          <Link className="navbar-brand" to="/"><img className="logo" src="/logo.png" width="340" height="70" className="d-inline-block align-top" alt="Image text"/></Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
+              <li className="nav-item px-4">
                 <Link className="nav-link" to='/doodles'><span className='ctext'>Hitos</span></Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item px-4">
                 <Link className="nav-link" to='/about'><span className='ctext'>Sobre Hitos</span></Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item px-4">
                 <Link className="nav-link" to='/register'><span className='ctext'>Signup</span></Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item px-4">
                 <Link className="nav-link" to='/login'><span className='ctext'>Login</span></Link>
               </li>
             </ul>
