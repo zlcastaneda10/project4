89f7acb1363c4b9e25a52251aae97936e7c5818f
Juan Pablo Gonzalez <jp.gonzalez14@uniandes.edu.co>
2018-10-23 18:09:57 -0500

    remove test method

--- server/main.js
+++ server/main.js
@@ -4,7 +4,5 @@ import './../imports/api/doodles';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  Meteor.call('greetUser', (err, res) => {
-    console.log('greet user arguments', err, res);
-  });
+  
 });
